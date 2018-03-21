2de9bfe67b42d48d13960818b7c0982093cc1a3a
王子龙 <wangzilong03@maoyan.com>
2018-03-21 11:57:12 +0800

    add: http cached related

--- examples/gulp-and-webpack/server/index.js
+++ examples/gulp-and-webpack/server/index.js
@@ -4,9 +4,18 @@ const app    = new Koa();
 const serve  = require('koa-static');
 
 
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
 // 静态文件服务目录
 const staticDir = path.join(__dirname, '../build');
-app.use(serve(staticDir));
+app.use(serve(staticDir, {
+  maxage: 360000,
+}));
 
 const port = 9001;
 app.listen(port);
