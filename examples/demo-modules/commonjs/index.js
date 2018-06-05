7a289a7fa7643c3fd788b604197bdb172930f8ba
王子龙 <809052335@qq.com>
2018-06-05 21:02:03 +0800

    haha add

--- examples/demo-modules/commonjs/index.js
+++ examples/demo-modules/commonjs/index.js
@@ -1,7 +1,6 @@
 var b1 = require('./b.js');
-var b2 = require('./b.js');
-
 var a = require('./a.js');
+var b2 = require('./b.js');
 
 console.log('b1 === b2 => ', b1 === b2);
 
--- examples/demo-modules/esm/my-app.mjs
+++ examples/demo-modules/esm/my-app.mjs
@@ -1,6 +1,9 @@
 import {name} from './lib.mjs';
 import {setName, add} from './lib.mjs';
 
+import './foo?query=1';
+import './foo?query=2';
+
 console.log(name);
 setName('李白');
 console.log(name);
